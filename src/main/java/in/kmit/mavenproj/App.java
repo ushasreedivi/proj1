15e3af57f47630991c662b82ea1977fb64cdbb0b
ushasreedivi2004@gmail.com <ushasreedivi2004@gmail.com>
2023-12-06 12:51:48 +0530

    completed

---
+++ src/main/java/in/kmit/mavenproj/App.java
@@ -0,0 +1,13 @@
+package in.kmit.mavenproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
